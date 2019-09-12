36739253d6b0f00a974988542afc3afbfe932d5e
AyanaZaire <ayanazaire@gmail.com>
2019-09-12 15:15:56 -0400

    added authentication helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enable sessions
+    enable :sessions
+    # set session secret for extra layer of security
+    set :session_secret, "our_super_secret_session_secret"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    # method to keep track of logged in user
+    def current_user
+      # find a the current user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+    # check to see if a user is logged in
+    def logged_in?
+      #will return a boolean (true or false)
+      !!current_user
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,26 @@ class UsersController < ApplicationController
 
   # this route actually logs our user in
   post "/login" do
-    # recieve params from form inputs
-    # find the user based on email params
-    # authenticate my user
-    # create a key/value pair in sessions hash ex: session[:user_id] = 1
+    # [x] recieve params from form inputs
+    # [x] find the user based on email params
+    user = User.find_by(email: params[:email])
+    # [x] authenticate my user
+    if user && user.authenticate(params[:password])
+      # [x] create a key/value pair in sessions hash (this is what actually logs the user in) ex: session[:user_id] = 1
+      session[:user_id] = user.id
+      # redirect to users profile
+      redirect "/users/#{user.id}"
+    else
+      # show login error message
+      # redirect back to the login
+      redirect "/login"
+    end
+  end
+
+  # users SHOW route
+  get "/users/:id" do
+    # find the user and save to instance
+    # render with access to user instance variable
   end
 
 end
